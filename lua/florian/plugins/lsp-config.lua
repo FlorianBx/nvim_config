1d7b3140339b29d06818af27750cca97e1997b5c
BFlorian91 <b.florian91@icloud.com>
2024-01-02 23:13:30 +0100

    feat: lsp add config

---
+++ lua/florian/plugins/lsp-config.lua
@@ -0,0 +1,70 @@
+local nvim_lsp_config = {
+  "neovim/nvim-lspconfig", -- Main LSP plugin
+  config = function()
+    local lspconfig = require("lspconfig")
+
+    -- Function to configure keymaps and other LSP-specific behaviors
+    local on_attach = function(client, bufnr)
+      local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+      end
+      local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+      end
+
+      -- Enable LSP-based completion
+      buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+      -- LSP keymaps
+      local opts = { noremap = true, silent = true }
+      buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+      buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+      buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+      buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+      buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+      buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+      buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+      buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+      buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+      buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+      buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+      buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+      buf_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+      buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+      buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+      buf_set_keymap("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+      buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    end
+
+    -- List of LSP servers to configure
+    local servers = { "volar", "tsserver", "tailwindcss", "html", "cssls", "lua_ls", "emmet_ls" }
+    for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup({
+        on_attach = on_attach,
+        flags = {
+          debounce_text_changes = 150,
+        },
+      })
+    end
+
+    -- Specific configuration for Lua LSP server
+    lspconfig.lua_ls.setup({
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+        },
+      },
+    })
+  end,
+}
+
+return nvim_lsp_config
