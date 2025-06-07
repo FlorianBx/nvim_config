2079746b9d606a928edf645085bd2590624baab8
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-06-07 05:15:33 +0200

    fix: some warnings and errors in LSP CONFIG

--- lua/florian/plugins/lsp/configs/common.lua
+++ lua/florian/plugins/lsp/configs/common.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 function M.setup()
-  -- Configuration Mason
   require("mason").setup({
     ui = {
       border = "rounded",
@@ -15,7 +14,7 @@ function M.setup()
 
   require("mason-lspconfig").setup({
     ensure_installed = {
-      "volar",           -- Vue
+      -- "volar",           -- Vue
       "tailwindcss",     -- Tailwind CSS
       "html",            -- HTML
       "cssls",           -- CSS
@@ -23,35 +22,9 @@ function M.setup()
     automatic_installation = true
   })
 
-  -- Keymaps LSP unifiées
-  vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-      local opts = { buffer = ev.buf }
-
-      -- LSP Navigation
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find all references" })
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Find implementations" })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation" })
-
-      -- Code Actions
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-
-      -- Diagnostics
-      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics" })
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-
-      -- References with improved UI
-      vim.keymap.set("n", "<leader>fr", "<cmd>Telescope lsp_references<cr>", { desc = "Find references with Telescope" })
-    end
-  })
-
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  
+
   return capabilities
 end
 
--- lua/florian/plugins/lsp/configs/typescript.lua
+++ lua/florian/plugins/lsp/configs/typescript.lua
@@ -1,28 +1,34 @@
--- Dans typescript.lua
 local M = {}
 
-function M.setup(capabilities)
-  require("typescript-tools").setup({
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-      vim.keymap.set("n", "gd", "<cmd>TSToolsGoToSourceDefinition<cr>", { buffer = bufnr })
-      vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = bufnr })
-      vim.keymap.set("n", "K", "<cmd>TSToolsHover<cr>", { buffer = bufnr })
-      vim.keymap.set("n", "<leader>ca", "<cmd>TSToolsFixAll<cr>", { buffer = bufnr })
-      vim.keymap.set("n", "<leader>rn", "<cmd>TSToolsRename<cr>", { buffer = bufnr })
-    end,
-    settings = {
-      tsserver_path = os.getenv("HOME") .. ".local/share/fnm/node-versions/v22.13.1/installation/lib/node_modules/typescript/lib",
-      complete_function_calls = true,
-      include_completions_with_insert_text = true,
-      tsserver_plugins = {},
-      tsserver_max_memory = "auto",
-      tsserver_format_options = {
-        allowIncompleteCompletions = true,
-        allowRenameOfImportPath = true,
-      },
-    }
+
+function M.setup()
+
+  vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
   })
+
+  -- require("lspconfig").typescript_tools.setup({
+  --   capabilities = capabilities,
+  -- })
+
+  -- local keymap = vim.keymap
+  -- local keymapOptionsWithDesc = function(desc)
+  --   return { noremap = true, silent = true, desc = desc }
+  -- end
+
+--   keymap.set("n", "<leader>to", "<cmd>TSToolsOrganizeImports<CR>", keymapOptionsWithDesc("Organize imports"))
+--   keymap.set("n", "<leader>ts", "<cmd>TSToolsSortImports<CR>", keymapOptionsWithDesc("Sort imports"))
+--   keymap.set("n", "<leader>tu", "<cmd>TSToolsRemoveUnusedImports<CR>", keymapOptionsWithDesc("Remove unused imports"))
+--   keymap.set("n", "<leader>tU", "<cmd>TSToolsRemoveUnused<CR>", keymapOptionsWithDesc("Remove all unused statements"))
+--   keymap.set("n", "<leader>tm", "<cmd>TSToolsAddMissingImports<CR>", keymapOptionsWithDesc("Add missing imports"))
+--   keymap.set("n", "<leader>tf", "<cmd>TSToolsFixAll<CR>", keymapOptionsWithDesc("Fix all fixable errors"))
+--   keymap.set("n", "<leader>tg", "<cmd>TSToolsGoToSourceDefinition<CR>", keymapOptionsWithDesc("Go to source definition"))
+--   keymap.set("n", "<leader>tr", "<cmd>TSToolsRenameFile<CR>", keymapOptionsWithDesc("Rename file and update references"))
+--   keymap.set("n", "<leader>tR", "<cmd>TSToolsFileReferences<CR>", keymapOptionsWithDesc("Find file references"))
 end
 
 return M
