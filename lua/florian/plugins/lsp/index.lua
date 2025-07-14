f9682088521a25645ee92ef4d6acec575be747ac
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-07-14 06:18:31 +0200

    fix: emmet was broken

--- lua/florian/plugins/lsp/index.lua
+++ lua/florian/plugins/lsp/index.lua
@@ -12,14 +12,21 @@ return {
 
       require("florian.plugins.lsp.configs.vue").setup(capabilities)
       require("florian.plugins.lsp.configs.typescript").setup(capabilities)
+      
+      vim.lsp.config.emmet_language_server = {
+        capabilities = capabilities,
+        filetypes = { "css", "html", "javascript", "javascriptreact", "less", "sass", "scss", "typescriptreact", "vue" },
+        init_options = {
+          includeLanguages = {
+            ["vue-html"] = "html",
+            ["vue"] = "html"
+          },
+          showAbbreviationSuggestions = true,
+          showExpandedAbbreviation = "always",
+        },
+      }
+      vim.lsp.enable('emmet_language_server')
     end
   },
 
-  {
-    "florianbx/ng-croissant",
-    ft = { "typescript", "javascript", "html" },
-    cond = function()
-      return vim.fn.glob("**/angular.json") ~= ""
-    end,
-  },
 }
