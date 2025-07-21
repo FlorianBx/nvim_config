58dee5e6ae0bbe56a8d5af3ca45f4902ea81abb0
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-07-21 18:47:19 +0200

    fix: lsp restore, with simple an modern features

--- lua/florian/plugins/lsp/index.lua
+++ lua/florian/plugins/lsp/index.lua
@@ -1,32 +1,83 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    "williamboman/mason.nvim",
     dependencies = {
-      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
     },
-    config = function()
-      local common = require("florian.plugins.lsp.configs.common")
-      local capabilities = common.setup()
-
-      require("florian.plugins.lsp.configs.vue").setup(capabilities)
-      require("florian.plugins.lsp.configs.typescript").setup(capabilities)
-      
-      vim.lsp.config.emmet_language_server = {
-        capabilities = capabilities,
-        filetypes = { "css", "html", "javascript", "javascriptreact", "less", "sass", "scss", "typescriptreact", "vue" },
-        init_options = {
-          includeLanguages = {
-            ["vue-html"] = "html",
-            ["vue"] = "html"
-          },
-          showAbbreviationSuggestions = true,
-          showExpandedAbbreviation = "always",
+    opts = {
+      server = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }
+              },
+            }
+          }
         },
+        vtsls = {
+          filetypes = { "javascript", "typescript", "vue" },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              tsserver = {
+                globalPlugins = {
+                  {
+                    name = "@vue/typescript-plugin",
+                    location = vim.fn.stdpath('data') .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                    languages = { "vue" },
+                    configNamespace = "typescript",
+                    enableForWorkspaceTypeScriptVersions = true,
+                  },
+                }
+              }
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true
+              }
+            }
+          }
+        },
+        emmet_ls = {
+          filetypes = { "html", "typescript", "javascript", "vue" },
+          init_options = {
+            html = {
+              options = {
+                ["vue.enabled"] = true
+              }
+            }
+          }
+        },
+        cssls = {},
+        oxlint = {},
+        jsonls = {},
+        tailwindcss = {}
       }
-      vim.lsp.enable('emmet_language_server')
-    end
-  },
+    },
+    config = function(_, opts)
+      require("mason").setup()
 
+      require("mason-lspconfig").setup({
+          ensure_installed = { "lua_ls", "vtsls", "vue-language-server", "html", "cssls", "jsonls", "tailwindcss", "emmet_ls" },
+      })
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        virtual_lines = false,
+        signs = true,
+        underline = true
+      })
+
+      for server, config in pairs(opts.server) do
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
+      end
+
+    end
+  }
 }
