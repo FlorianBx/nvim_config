8e18c4e81bda931ea4ffa4812f1cd3de064fc3e3
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-08-28 00:06:23 +0200

    fix: try new shortcut to auto-complete

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -10,7 +10,7 @@ return {
         ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
         ['<C-e>'] = { 'hide' },
         -- TODO: found a better shortcut for accept
-        ['<C-y'] = { 'select_and_accept' },
+        -- ['<C-y'] = { 'select_and_accept' },
 
         ['<C-p>'] = { 'select_prev', 'fallback' },
         ['<C-n>'] = { 'select_next', 'fallback' },
@@ -18,7 +18,7 @@ return {
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
-        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<Tab>'] = { 'select_and_accept', 'fallback' },
         ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
       },
 
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,7 +8,7 @@ return {
       enabled = true,
       auto_trigger = true,
       keymap = {
-        accept = "<Tab>",
+        accept = "<S-Tab>",
         next = "<M-]>",
         prev = "<M-[>",
       },
