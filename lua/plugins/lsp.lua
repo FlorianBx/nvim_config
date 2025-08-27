b8d1aeb229fa2bcc025ab6492fc41f7d5201bbd3
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2025-08-27 13:59:00 +0200

    feat: add pretty hover

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
 
     local on_attach = function(_, bufnr)
       local opts = { buffer = bufnr, silent = true }
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'K', function() require("pretty_hover").hover() end, opts)
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
